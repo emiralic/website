f7e1363ed4190d07b9ce59105ca99109f22c16a2
Emir <emiralic22@gmail.com>
2022-12-21 00:34:46 +0100

    Third

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
@@ -10,6 +10,27 @@ def login():
 def logout():
     return "<p>Logout</p>"
 
-@auth.route('/register')
+@auth.route('/register', methods=['GET', 'POST'])
 def register():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        ime = request.form.get('ime')
+        prezime = request.form.get('prezime')
+        lozinka1 = request.form.get('lozinka1')
+        lozinka2 = request.form.get('lozinka2')
+
+
+        if len(ime) < 2:
+            flash('Ime mora sadržavati više od 1 slova.', category='error')
+        elif len(prezime) < 2:
+            flash('Prezime mora sadržavati više od 1 slova.', category = 'error')
+        elif len(email) < 4:
+            flash('Email adresa mora sadržavati više od 3 slova.', category='error')
+        elif lozinka1 != lozinka2:
+            flash('Ponovljena lozinka mora biti identična kao unešena lozinka.', category = 'error')
+        elif len(lozinka1) < 6:
+            flash('Lozinka mora sadržavati više od 5 znakova.', category = 'error')
+        else:
+            flash('Profil uspješno kreiran.', category = 'success')
+
     return render_template('register.html')
--- website/views.py
+++ website/views.py
@@ -3,5 +3,10 @@ from flask import Blueprint, render_template
 views = Blueprint('views', __name__)
 
 @views.route('/')
+@views.route('home')
 def home():
     return render_template('home.html')
+
+@views.route('/youtube')
+def youtube():
+    return render_template('youtube.html')
